20b7589fa06ac9122f01e14dc30513dd7b2c9872
Heitor Danilo <heitor.danilo@icloud.com>
2023-12-29 14:57:27 -0300

    standard modkeys

--- src/util.rs
+++ src/util.rs
@@ -3,6 +3,18 @@ pub fn to_keysym(ch: char) -> u32 {
     ch as u32
 }
 
+#[allow(dead_code)]
+pub mod modkeys {
+    pub const MODKEY_1: u16 = xcb::MOD_MASK_1 as u16;
+    pub const MODKEY_2: u16 = xcb::MOD_MASK_2 as u16;
+    pub const MODKEY_3: u16 = xcb::MOD_MASK_3 as u16;
+    pub const MODKEY_4: u16 = xcb::MOD_MASK_4 as u16;
+    pub const MODKEY_ANY: u16 = xcb::MOD_MASK_ANY as u16;
+    pub const MODKEY_LOCK: u16 = xcb::MOD_MASK_LOCK as u16;
+    pub const MODKEY_SHIFT: u16 = xcb::MOD_MASK_SHIFT as u16;
+    pub const MODKEY_CONTROL: u16 = xcb::MOD_MASK_CONTROL as u16;
+}
+
 /// NOTE:
 /// For now, sapphire does not support multiple monitors and due to rust's
 /// lifetimes and how xcb::Screen needs conn, it's really hard to use screen
